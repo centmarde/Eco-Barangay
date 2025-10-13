c8ffb502f929cd86f11fd67fcb1c9004db985f26
Jetross <jetrossneri07@gmail.com>
2025-10-13 08:48:44 +0800

    notification modal on small screens are now improved

--- src/components/common/NotificationBell.vue
+++ src/components/common/NotificationBell.vue
@@ -14,6 +14,7 @@ const { mobile } = useDisplay();
 
 // Local state
 const notificationMenu = ref(false);
+const mobileMenu = ref(false);
 let realtimeChannel: RealtimeChannel | null = null;
 
 // Computed properties
@@ -33,6 +34,7 @@ const markAsRead = async (notificationId: number) => {
 
 const viewAllNotifications = () => {
   notificationMenu.value = false;
+  mobileMenu.value = false;
   router.push("/notifications"); // TODO: Update to actual notifications page route
 };
 
@@ -45,8 +47,13 @@ const handleNotificationClick = async (notification: any) => {
   // Navigate if there's an action URL
   if (notification.action_url) {
     notificationMenu.value = false;
+    mobileMenu.value = false;
     router.push(notification.action_url);
   }
+};
+
+const openMobileMenu = () => {
+  mobileMenu.value = true;
 };
 
 // Lifecycle hooks
@@ -90,9 +97,6 @@ onUnmounted(() => {
         >
           <v-icon icon="mdi-bell" />
         </v-badge>
-        <v-tooltip activator="parent" location="bottom">
-          Notifications
-        </v-tooltip>
       </v-btn>
     </template>
 
@@ -168,24 +172,101 @@ onUnmounted(() => {
     </v-card>
   </v-menu>
 
-  <!-- Mobile Notification Item (for drawer) -->
-  <v-list-item
+  <!-- Mobile Notification Bell - Menu Style -->
+  <v-menu
     v-else
-    title="Notifications"
-    prepend-icon="mdi-bell"
-    rounded="xl"
-    class="ma-2"
-    @click="viewAllNotifications"
+    v-model="mobileMenu"
+    :close-on-content-click="false"
+    location="bottom end"
+    offset="8"
+    max-width="400"
   >
-    <template #append>
-      <v-badge
-        v-if="unreadCount > 0"
-        :content="unreadCount"
-        color="error"
-        inline
-      />
+    <template #activator="{ props: menuProps }">
+      <v-btn v-bind="menuProps" icon variant="text" size="small">
+        <v-badge
+          :content="unreadCount"
+          :model-value="unreadCount > 0"
+          color="error"
+          overlap
+        >
+          <v-icon icon="mdi-bell" size="20" />
+        </v-badge>
+      </v-btn>
     </template>
-  </v-list-item>
+
+    <v-card min-width="350" max-width="400">
+      <v-card-title class="d-flex justify-space-between align-center pa-4">
+        <span class="text-h6">Notifications</span>
+        <v-btn icon variant="text" size="small" @click="mobileMenu = false">
+          <v-icon>mdi-close</v-icon>
+        </v-btn>
+      </v-card-title>
+
+      <v-divider />
+
+      <v-list class="pa-0" max-height="400" style="overflow-y: auto">
+        <template v-if="notifications.length > 0">
+          <v-list-item
+            v-for="notification in notifications"
+            :key="notification.id"
+            :class="{ 'bg-surface-light': !notification.read }"
+            class="px-4 py-3 cursor-pointer"
+            @click="handleNotificationClick(notification)"
+          >
+            <template #prepend>
+              <v-avatar
+                :color="getNotificationColor(notification)"
+                size="40"
+                variant="tonal"
+              >
+                <v-icon :icon="getNotificationIcon(notification.type)" />
+              </v-avatar>
+            </template>
+
+            <v-list-item-title class="font-weight-medium mb-1">
+              {{ notification.title }}
+            </v-list-item-title>
+            <v-list-item-subtitle class="text-wrap">
+              {{ notification.message }}
+            </v-list-item-subtitle>
+            <v-list-item-subtitle class="text-caption mt-1">
+              {{ notificationsStore.getRelativeTime(notification.created_at) }}
+            </v-list-item-subtitle>
+          </v-list-item>
+        </template>
+
+        <div v-else class="text-center py-8">
+          <v-icon size="48" color="grey-lighten-1" class="mb-2">
+            mdi-bell-off-outline
+          </v-icon>
+          <p class="text-body-2 text-medium-emphasis">No notifications</p>
+        </div>
+      </v-list>
+
+      <v-divider />
+
+      <v-card-actions class="pa-3 d-flex flex-column ga-2">
+        <v-btn
+          v-if="unreadCount > 0"
+          block
+          variant="text"
+          color="primary"
+          size="small"
+          @click="markAllAsRead"
+        >
+          Mark all as read
+        </v-btn>
+        <v-btn
+          block
+          variant="tonal"
+          color="primary"
+          @click="viewAllNotifications"
+        >
+          View All Notifications
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-menu>
 </template>
 
 <style scoped>
